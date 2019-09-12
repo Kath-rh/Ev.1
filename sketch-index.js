ac3503e4eab3862bbed3d2c9286da4736eaa9250
Kath-rh <53885066+Kath-rh@users.noreply.github.com>
2019-09-12 16:56:36 -0300

    Update sketch-index.js

--- sketch-index.js
+++ sketch-index.js
@@ -1,20 +1,35 @@
+var r;
+
 function setup() {
-  noCanvas();
+    var Canvas = createCanvas(windowWidth, windowHeight);
+    canvas.position(0, 0);
+    canvas.style('z-index', -1);
+    r = random(0, 360);
+    colorMode(HSB, 250, 60, 60);
+    background(r, 100, 100);
 }
+
 function saluda() {
     var nombre = document.getElementById("nombre").value;
-  document.getElementsByTagName("h1")[0].innerText="La vida es para ser feliz, " + nombre + " los astros te dicen";
+    document.getElementsByTagName("h1")[0].innerText = "La vida es para ser feliz, " + nombre + " los astros te dicen";
     var edad = document.getElementById("edad").value;
     var signo = document.getElementById("signo").value;
 
     var tamano = 30
-    if (edad < 20 || edad > 60){
-      depende = "oye"
-    }else{
-      depende = "baia baia"
+    if (edad < 20 || edad > 60) {
+        depende = "oye"
+    } else {
+        depende = "baia baia"
     }
-    document.getElementsByTagName("h2")[0].innerText = depende;
-    var signo = document.getElementById("signo").value;
-    document.getElementsByTagName("h3")[0].innerText = signo + " más";
 
-}
+    function draw() {
+        inventa(random(1, 100));
+    }
+
+    function inventada(diametro) {
+        colorMode(HSB, 250, 60, 60);
+        stroke(r, 100, 75);
+        colorMode(RGB);
+        fill(100, 90)
+        ellipse(mouseX, mouseY, diametro, diametro);
+    }
