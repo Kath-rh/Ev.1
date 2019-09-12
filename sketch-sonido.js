0f935825e42cd4228413b8f910619eaa4dff08cd
Kath-rh <53885066+Kath-rh@users.noreply.github.com>
2019-09-12 16:57:28 -0300

    Update sketch-sonido.js

--- sketch-sonido.js
+++ sketch-sonido.js
@@ -3,19 +3,19 @@ function setup() {
  createCanvas(windowWidth, windowHeight);
  frameRate(5);
  noStroke();
- colorMode(HSB, 360, 100, 100);
+ colorMode(HSB, 360, 150, 40);
  mic = new p5.AudioIn()
  mic.start();
 }
 
 function draw() {
  micLevel = mic.getLevel();
-  var co = micLevel*800
+  var co = micLevel*2000
 
-   var a = constrain(micLevel*900, 0, 360);
-   var b = constrain(micLevel*4000, 32,255)
-   var c = constrain(micLevel*50000, 12, 180)
-   square(random(micLevel*5000,0,width), random(0,height), random(1,co));
+   var a = constrain(micLevel*5000, 0, 360);
+   var b = constrain(micLevel*3000, 32,255)
+   var c = constrain(micLevel*1000, 12, 180)
+   square(random(micLevel*5000,0,width), random(1,height), random(0,co));
    fill(a, b, c);
 
 }
